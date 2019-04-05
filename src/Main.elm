c2a69e47c19d53e672dc8dc8d566563a647e30b7
Makoto Ishida <makotoishida@gmail.com>
2019-04-04 14:03:14 -1000

    オンスクリーンキーボードの実装。文字数カウンタ表示。

--- src/Main.elm
+++ src/Main.elm
@@ -10,8 +10,15 @@ import Html.Events exposing (onClick)
 import Url
 import Json.Decode as Decode
 
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-letters = ["A", "B", "C"]
+keys = [
+  "QWERTYUIOP",
+  "ASDFGHJKL",
+  "ZXCVBNM",
+  " "
+  ]
+
+max_length = 90
+
 
 keyDecoder : Decode.Decoder Msg
 keyDecoder =
@@ -21,7 +28,7 @@ toKey : String -> Msg
 toKey string =
   case String.uncons string of
     Just (char, "") ->
-      AddLetter (String.fromChar char)
+      AddLetter (String.toUpper (String.fromChar char))
 
     _ ->
       Control string
@@ -41,12 +48,15 @@ type Msg
   = AddLetter String
   | Control String
   | Reset
+  | EncryptClick
+  | Encrypt String
   | LinkClicked Browser.UrlRequest
   | UrlChanged Url.Url
 
 type alias Model = {
     rawStr : String,
     encryptedStr: String,
+    isKeyDialogOpen: Bool,
     key : Nav.Key,
     url : Url.Url
   }
@@ -60,22 +70,35 @@ init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key = ({
     rawStr = "",
     encryptedStr = "",
+    isKeyDialogOpen = False,
     key = key,
     url = url
   }, Cmd.none)
-
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     AddLetter letter ->
-      ({ model | rawStr = model.rawStr ++ letter }, Cmd.none)
+      ({ model |
+          rawStr =
+            (if (String.length model.rawStr) < max_length then
+              model.rawStr ++ letter
+            else
+              model.rawStr
+            )
+      }, Cmd.none)
 
     Control _ ->
       (model, Cmd.none)
 
     Reset ->
       ({ model | rawStr = "" }, Cmd.none)
+
+    EncryptClick ->
+      ({model | isKeyDialogOpen = True }, Cmd.none)
+
+    Encrypt key ->
+      (model, Cmd.none)
 
     LinkClicked urlRequest ->
       case urlRequest of
@@ -90,26 +113,49 @@ update msg model =
       , Cmd.none
       )
 
-alphabetToLetters: String -> List Char
-alphabetToLetters str = String.toList alphabet
-
 lettersToButtons: List Char -> List (Html Msg)
 lettersToButtons list =
   List.map (\x ->
-    button [onClick (AddLetter (String.fromChar x))]
-    [ text (String.fromChar x)]) list
+    button [
+        class (if x == ' ' then "space" else "key"),
+        onClick (AddLetter (String.fromChar x))
+      ]
+      [
+        text (String.fromChar x)
+      ]
+  ) list
 
+renderKeyboard : Html Msg
+renderKeyboard =
+  div [ class "keyboard" ] (List.map (\s ->
+    div [] (lettersToButtons (String.toList s))) keys)
+
+renderKeyDialog : Model -> Html Msg
+renderKeyDialog model =
+  if model.isKeyDialogOpen == False then
+    div [] []
+  else
+    div [] [ text "renderKeyDialog" ]
 
 view : Model -> Browser.Document Msg
 view model =
   { title = "My Secret Message"
   , body = [
-      div [ class "main" ] [
-        div [] [ text model.rawStr, text " / ",  text (String.fromInt (String.length model.rawStr)) ]
-        , button [ class "reset", onClick Reset ] [ text "Reset" ]
-        , br [] []
-        , div [] (lettersToButtons (alphabetToLetters alphabet))
-      ]
+      div [ class "main" ]
+        [
+          div [ class "input" ]
+            [
+              text model.rawStr
+            ]
+          , div [ class "counter" ]
+            [
+              text (String.fromInt (max_length - String.length model.rawStr))
+              , button [ class "reset", onClick Reset ] [ text "CLEAR" ]
+              , button [ class "encrypt", onClick EncryptClick ] [ text "ENCRYPT / DECRYPT" ]
+            ]
+          , renderKeyboard
+          , renderKeyDialog model
+        ]
     ]
   }
 
